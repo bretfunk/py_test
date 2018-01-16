4581d3eecfdf82bb8f26a6a9aa3f7339fd556424
BFunk <bret.e.funk@gmail.com>
2018-01-16 13:39:56 -0500

    added variables

--- lib/main.py
+++ lib/main.py
@@ -1,13 +1,17 @@
 from gtts import gTTS
 import boto3
 
+phrase = "test phrase"
+language = "en"
+file_name = "test_filename"
 
-#already working, DONT DELETE
-tts = gTTS(text='Hello', lang='en')
-tts.save("./temp/hello.mp3")
+
+#create file
+tts = gTTS(text=phrase, lang=language)
+tts.save("./temp/{}/{}.mp3".format(language, file_name))
 
 bucket = 'elasticbeanstalk-us-west-2-747213477632'
 s3_client = boto3.client('s3')
 
 # Upload the file to S3
-s3_client.upload_file('./temp/hello.mp3', bucket, 'hello-remote.mp3')
+s3_client.upload_file("./temp/{}/{}.mp3".format(language, file_name), bucket, "{}/{}.mp3".format(language, file_name))
